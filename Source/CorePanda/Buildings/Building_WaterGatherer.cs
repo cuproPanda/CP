74962d23b4ab6477312c1b58487e348328953248
cuproPanda <cupropanda@gmail.com>
2016-08-12 09:33:04 -0400

    Well implemented as derived class

---
+++ Source/CorePanda/Buildings/Building_WaterGatherer.cs
@@ -0,0 +1,113 @@
+﻿using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace CorePanda {
+
+  internal class Building_WaterGatherer : Building_WorkTable {
+
+    protected float containedWaterInt = 0f;
+    protected readonly float bucketVolume = 1200f;
+    protected bool hasEnoughWater = false;
+    protected float biomeMultiplier;
+
+    protected virtual float maxWater { get; }
+
+    public float ContainedWater {
+      get { return containedWaterInt; }
+    }
+
+
+    public override void ExposeData() {
+      base.ExposeData();
+      Scribe_Values.LookValue(ref containedWaterInt, "containedWater", 0f, false);
+      Scribe_Values.LookValue(ref hasEnoughWater, "hasEnoughWater", false, false);
+    }
+
+
+    public override void SpawnSetup() {
+      base.SpawnSetup();
+
+      Log.Warning(def.LabelCap + " spawned at " + Position.ToIntVec2);
+
+      // Get the map's rainfall, at a min of 100 and a max of 1000,
+      // then divide by 1000. Values will be somewhere between 0.1f and 1.0f
+      biomeMultiplier = Mathf.Min(Mathf.Max(Find.Map.WorldSquare.rainfall, 100), 1000f) / 1000;
+    }
+
+
+    public void BucketSpawned(int buckets = 1) {
+      // Make sure there is enough water to remove. Do nothing, but send a warning
+      if (containedWaterInt - (bucketVolume * buckets) < 0) {
+        Log.Warning("CorePanda:: Drawing more water from the " + def.label + " at " + Position.ToIntVec2 + " than it was supposed to have.");
+      }
+
+      // Remove 1200 water per bucket
+      AddOrRemoveWater(-bucketVolume * buckets);
+
+      // If there's less than 1200 water remaining, there's not enough water to fill another bucket
+      if (containedWaterInt < bucketVolume) {
+        hasEnoughWater = false;
+      }
+    }
+
+
+    protected void EmptyContainer() {
+      Thing bucket = Position.GetThingList().Find(b => b.def == ThingDef.Named("CP_FreshWaterBucket"));
+
+      if (bucket != null && bucket.stackCount >= 10) {
+        Messages.Message("CP_TooManyBuckets".Translate(new object[] { def.label }), Position, MessageSound.Negative);
+        return;
+      }
+
+      if (bucket != null && bucket.stackCount < 10) {
+        for (int b = 0; b < (10 - bucket.stackCount); b++) {
+          if (hasEnoughWater) {
+            bucket.stackCount++;
+            BucketSpawned();
+          }
+        }
+        bucket.SetForbidden(true, false);
+      }
+
+      if (bucket == null) {
+        Thing newBucket = ThingMaker.MakeThing(ThingDef.Named("CP_FreshWaterBucket"));
+        newBucket.stackCount = Mathf.Min(Mathf.FloorToInt(containedWaterInt / bucketVolume), newBucket.def.stackLimit);
+        GenSpawn.Spawn(newBucket, Position);
+        Position.GetThingList().Find(b => b.def == ThingDef.Named("CP_FreshWaterBucket")).SetForbidden(true, false);
+        BucketSpawned(newBucket.stackCount);
+      }
+    }
+
+
+    protected void AddOrRemoveWater(float amount, bool affectedByBiome = false) {
+      if (affectedByBiome) {
+        amount *= biomeMultiplier;
+      }
+      containedWaterInt = Mathf.Clamp(containedWaterInt + amount, 0, maxWater);
+    }
+
+
+    public override string GetInspectString() {
+      StringBuilder stringBuilder = new StringBuilder();
+      // Get inherited string data
+      stringBuilder.Append(base.GetInspectString());
+
+      // Display the water level
+      stringBuilder.AppendLine("CP_WaterLevel".Translate() + ": " + containedWaterInt.ToString("####0"));
+
+      // Display whether there's enough water to fill a bucket
+      stringBuilder.Append("CP_Buckets".Translate() + ": ");
+      if (hasEnoughWater) {
+        stringBuilder.AppendLine(Mathf.FloorToInt(containedWaterInt / bucketVolume).ToString());
+      }
+      if (!hasEnoughWater) {
+        stringBuilder.AppendLine("0");
+      }
+
+      return stringBuilder.ToString();
+    }
+  }
+}
--- Source/CorePanda/Buildings/Building_Well.cs
+++ Source/CorePanda/Buildings/Building_Well.cs
@@ -1,29 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.Text;
 
 using UnityEngine;
-using RimWorld;
 using Verse;
 
 namespace CorePanda {
 
-  internal class Building_Well : Building_WorkTable {
+  internal class Building_Well : Building_WaterGatherer {
 
-    private readonly float maxWater = 12000f;
-    private float containedWaterInt = 0f;
-    private bool hasEnoughWater = false;
-    private float biomeMultiplier;
     private CompColdPusher coldpusherComp;
 
-    public float ContainedWater {
-      get { return containedWaterInt; }
-    }
-
-
-    public override void ExposeData() {
-      base.ExposeData();
-      Scribe_Values.LookValue(ref containedWaterInt, "containedWater", 0f, false);
-      Scribe_Values.LookValue(ref hasEnoughWater, "hasEnoughWater", false, false);
+    protected override float maxWater {
+      get { return bucketVolume * 20; }
     }
 
 
@@ -32,12 +19,8 @@ namespace CorePanda {
 
       coldpusherComp = GetComp<CompColdPusher>();
 
-      // Get the map's rainfall, at a min of 100 and a max of 1000,
-      // then divide by 1000. Values will be somewhere between 0.1f and 1.0f
-      biomeMultiplier = Mathf.Min(Mathf.Max(Find.Map.WorldSquare.rainfall, 100), 1000f) / 1000;
-
       // Add beginning water, so the well doesn't start completely dry
-      AddOrRemoveWater(1500 * biomeMultiplier);
+      AddOrRemoveWater(bucketVolume * 1.3f, true);
     }
 
 
@@ -62,7 +45,7 @@ namespace CorePanda {
           defaultLabel = "CP_EmptyWell".Translate(),
           defaultDesc = "CP_EmptyWellDesc".Translate(),
           activateSound = SoundDef.Named("Click"),
-          action = () => { EmptyWell(); },
+          action = () => { EmptyContainer(); },
         };
         yield return emptyWell; 
       }
@@ -75,57 +58,15 @@ namespace CorePanda {
     }
 
 
-    public void BucketSpawned(int buckets = 1) {
-      // Make sure there is enough water to remove. Do nothing, but send a warning
-      if (containedWaterInt - (1200f * buckets) < 0 ) {
-        Log.Warning("CorePanda:: Drawing more water from the well than it was supposed to have.");
-      }
-
-      // Remove 1200 water per bucket
-      AddOrRemoveWater(-1200f * buckets);
-
-      // If there's less than 1200 water remaining, there's not enough water to fill another bucket
-      if (containedWaterInt < 1200f) {
-        hasEnoughWater = false;
-      }
-    }
-
-
     public override void Tick() {
       base.Tick();
-
-      if (Position.GetTemperature() > coldpusherComp.Props.ColdPushMinTemperature) {
-        coldpusherComp.SimplePush(60f / biomeMultiplier);
-      }
 
       if (Find.TickManager.TicksGame % 25 == 0) {
         CalculateWater();
       }
-    }
 
-
-    private void EmptyWell() {
-      Thing bucket = Position.GetThingList().Find(b => b.def == ThingDef.Named("CP_FreshWaterBucket"));
-
-      if (bucket != null && bucket.stackCount >= 10) {
-        Messages.Message("CP_TooManyBuckets".Translate(), Position, MessageSound.Negative);
-        return;
-      }
-
-      if (bucket != null && bucket.stackCount < 10) {
-        while (hasEnoughWater) {
-          bucket.stackCount++;
-          BucketSpawned();
-        }
-        bucket.SetForbidden(true, false);
-      }
-
-      if (bucket == null) {
-        Thing newBucket = ThingMaker.MakeThing(ThingDef.Named("CP_FreshWaterBucket"));
-        newBucket.stackCount = Mathf.FloorToInt(containedWaterInt / 1200f);
-        GenSpawn.Spawn(newBucket, Position);
-        Position.GetThingList().Find(b => b.def == ThingDef.Named("CP_FreshWaterBucket")).SetForbidden(true, false);
-        BucketSpawned(newBucket.stackCount);
+      if (Position.GetTemperature() > coldpusherComp.Props.ColdPushMinTemperature) {
+        coldpusherComp.SimplePush(60f / biomeMultiplier);
       }
     }
 
@@ -147,36 +88,9 @@ namespace CorePanda {
       AddOrRemoveWater(0.8f * biomeMultiplier);
 
       // If the well has 1200 water or more, there's enough water to fill another bucket
-      if (containedWaterInt >= 1200f) {
+      if (containedWaterInt >= bucketVolume) {
         hasEnoughWater = true;
       }
-    }
-
-
-    private void AddOrRemoveWater(float amount) {
-      containedWaterInt = Mathf.Clamp(containedWaterInt + amount, 0, maxWater);
-    }
-
-
-    /// <summary></summary>
-    public override string GetInspectString() {
-      StringBuilder stringBuilder = new StringBuilder();
-      // Get inherited string data
-      stringBuilder.Append(base.GetInspectString());
-
-      // Display the water level
-      stringBuilder.AppendLine("CP_WaterLevel".Translate() + ": " + containedWaterInt.ToString("####0"));
-
-      // Display whether there's enough water to fill a bucket
-      stringBuilder.Append("CP_Buckets".Translate() + ": ");
-      if (hasEnoughWater) {
-        stringBuilder.AppendLine(Mathf.FloorToInt(containedWaterInt / 1200).ToString());
-      }
-      if (!hasEnoughWater) {
-        stringBuilder.AppendLine("0");
-      }
-
-      return stringBuilder.ToString();
     }
   }
 }
